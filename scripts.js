cb6236c9f3142ba6d12d466bd4fd20030e9c024c
gianpaolo papaiz <gipppppa@hotmail.com>
2021-02-12 09:59:26 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,28 @@
+let logo = document.getElementById('logo');
+
+function whiteRabbit() {
+    let paragraph = document.createElement('p');
+    let position = document.getElementById('banner-text');
+    paragraph.innerHTML = 'Follow the white rabbit ...';
+    position.appendChild(paragraph);
+    logo.removeEventListener('click', whiteRabbit);   
+}
+
+logo.addEventListener('click', whiteRabbit);
+
+
+
+
+let myName = document.getElementById('name');
+let initialSize = myName.style.fontSize;
+
+function grow() {
+    myName.style.fontSize =  '3rem';
+    myName.removeEventListener('mouseenter', grow); 
+}
+
+myName.addEventListener('mouseenter', grow);
+
+
+
+
